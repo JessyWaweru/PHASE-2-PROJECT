04377113bad0c4a87d2ae005057dead339062628
nimo <nimo.sheikh@student.moringaschool.com>
2023-05-04 21:47:08 +0300

    professional changes

--- random-dog-images/src/App.js
+++ random-dog-images/src/App.js
@@ -1,30 +1,59 @@
-
-import './App.css';
-import React from 'react'
-import {BrowserRouter as Router,Routes,Route}from 'react-router-dom'
-import Header from './components/Header'
-import Footer from './components/Footer'
-import Home from './components/Home'
-import Breed from './components/Breed'
-import NotFound from './components/NotFound'
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  useLocation,
+} from "react-router-dom";
+// import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./components/Home";
+import Breed from "./components/Breed";
+import NotFound from "./components/NotFound";
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
+import LandingPageNav from "./components/LandingPageNav";
+import HomeNavBar from "./components/HomePageNav";
+import LandingPage from "./components/Landing";
+import "./App.css";
 
 function App() {
-return(
-<Router>
-  <h1 className='h1-1'> MARTHA'S PET STORE</h1>
-<Header/>
-<Routes>
-  <Route path='/' element={<Home/>}/>
-  <Route path='/breed/:breed' element={<Breed/>}/>
-  <Route path='*' element={<NotFound/>}/>
-</Routes>
-<Footer/>
+  const location = useLocation();
+  const path = location.pathname;
+  let navbar;
 
-</Router>
+  if (
+    path === "/" ||
+    path === "/login" ||
+    path === "/register" ||
+    path === "/about" ||
+    path === "/services" ||
+    path === "/footer"
+  ) {
+    navbar = <LandingPageNav />;
+  } else {
+    navbar = <HomeNavBar />;
+  }
 
-)
+  return (
+    <Router>
+      {navbar}
+      {/* <Header /> */}
+      <div style={{top: "10em", bottom: "10em"}}>
+        {" "}
+        <Switch>
+          <Route exact path="/" component={LandingPage} />{" "}
+          <Route exact path="/home" component={Home} />
+          <Route path="/breed/:breed" component={Breed} />
+          <Route path="/login" component={Login} />
+          <Route path="/register" component={Register} />
+          <Route component={NotFound} />
+        </Switch>
+      </div>
 
-
+      <Footer />
+    </Router>
+  );
 }
- 
+
 export default App;
