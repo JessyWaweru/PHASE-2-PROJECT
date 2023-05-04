584bb69f59ee83897b471978f0ee1fecce31a146
Martha <martha.maina@student.moringaschool.com>
2023-05-04 12:45:19 +0300

    made changes

--- random-dog-images/src/components/Home.js
+++ random-dog-images/src/components/Home.js
@@ -1,42 +1,49 @@
-import React,{useState,useEffect} from 'react'
-import {Link}from 'react-router-dom'
-import axios from 'axios'
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 
-function Home (){
-    const[breeds,setBreeds]=useState([])
-    useEffect(()=>{
-        axios.get('https://dog.ceo/api/breeds/list/all')
-        .then(res=>{
-            const breeds=Object.keys(res.data.message)
-            setBreeds(breeds)
-        })
-        .catch(error=>{console.log(error)})
-    },[])
-    
+function Home() {
+  const [breeds, setBreeds] = useState([]);
+  const [imageUrl, setImageUrl] = useState('');
 
-  const adoptRescueDog=()=>{
-        axios.get('https://dog.ceo/api/breeds/image/random')
-        .then(res=>{
-            const imageUrl=res.data.message
-        
-            
-        })
-        .catch(error=>{console.log(error)
-        })
-    }
-    return(
-        <div className='home'>
-            <h2>
-                Select a breed from our store or adopt our rescued pets:
-            </h2>
-            <div className='breeds'>
-                {breeds.map(breed=>(
-                    <Link key={breed} to={`/breed/${breed}`}>{breed}</Link>
-                ))}
-            </div>
-            <button onClick={adoptRescueDog}>ADOPT A RESCUE DOG HERE!!! </button>
-        </div>
-    )
+  useEffect(() => {
+    axios
+      .get('https://dog.ceo/api/breeds/list/all')
+      .then((res) => {
+        const breeds = Object.keys(res.data.message);
+        setBreeds(breeds);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  const adoptRescueDog = () => {
+    axios
+      .get('https://dog.ceo/api/breeds/image/random')
+      .then((res) => {
+        const imageUrl = res.data.message;
+        setImageUrl(imageUrl); // Set the value of imageUrl to the component state
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  return (
+    <div className="home">
+      <h2>Select a breed from our store or adopt our rescued pets:</h2>
+      <div className="breeds">
+        {breeds.map((breed) => (
+          <Link key={breed} to={`/breed/${breed}`}>
+            {breed}
+          </Link>
+        ))}
+      </div>
+      <button onClick={adoptRescueDog}>ADOPT A RESCUE DOG HERE!!!</button>
+      {imageUrl && <img src={imageUrl} alt="Rescue Dog" />} {/* Render the image if imageUrl is not empty */}
+    </div>
+  );
 }
 
-export default Home
+export default Home;
