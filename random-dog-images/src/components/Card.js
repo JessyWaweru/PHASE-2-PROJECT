f9e85e5af4e3c6e35c7d100a3079f33214dd78ae
Eric Mugo <mugoericpg20@gmail.com>
2023-05-04 15:29:16 +0300

    Removed Card component from Home.js

--- random-dog-images/src/components/Card.js
+++ random-dog-images/src/components/Card.js
@@ -2,9 +2,15 @@ import React from 'react';
 
 function Card(){
 
-    fetch ('')
+    fetch ('ttp://localhost:3000/dogs')
+    .then(r => r.json())
+    .then(data => console.log(data))
+    .catch(error => console.error(error))
 
 
 
     
 }
+
+
+export default Card;
--- random-dog-images/src/components/Home.js
+++ random-dog-images/src/components/Home.js
@@ -54,18 +54,7 @@ function Home() {
 
 export default Home;
 
-// added card
 
-<div class="card">
-  <div class="card-border-top">
-  </div>
-  <div class="img">
-  </div>
-  <span> Person</span>
-  <p class="job"> Job Title</p>
-  <button> Click
-  </button>
-</div>
 
 
 
